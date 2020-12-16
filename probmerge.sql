77f6021956276a1ce714bf541cbf0fb2b78dce9e
Maurice van Keulen <m.vankeulen@utwente.nl>
2020-12-16 09:54:58 +0100

    attempt to create bdds (resulting in a crash)

--- probmerge.sql
+++ probmerge.sql
@@ -7,10 +7,12 @@ CREATE TABLE _dict (name VARCHAR(20), dict DICTIONARY);
 -- create the 'wdc_dict' dictionary
 INSERT INTO _dict(name,dict) VALUES('wdc_dict',dictionary(''));
 
+DROP MATERIALIZED VIEW IF EXISTS denorm_p_name;
 DROP VIEW IF EXISTS p_name_alts;
 DROP VIEW IF EXISTS p_name_vars;
 DROP VIEW IF EXISTS denorm_p_name_raw;
 
+-- View for the denormalized table containing all values for only the attribute p_name
 CREATE VIEW denorm_p_name_raw AS
 	SELECT *, ROW_NUMBER() OVER () AS p_name_key
 	FROM (
@@ -21,6 +23,8 @@ CREATE VIEW denorm_p_name_raw AS
 	) dpnr
 ;
 
+-- View for establishing the necessary random variables for the uncertainty
+-- caused by conflicting p_name values within each cluster
 CREATE VIEW p_name_vars AS
 	SELECT *, 'pn'||varnum AS var
 	FROM (
@@ -32,6 +36,8 @@ CREATE VIEW p_name_vars AS
 		) AS b
 ;
 
+-- View for the construction of the associated random variable assignments
+-- associated with each alternative value of p_name for each cluster
 CREATE VIEW p_name_alts AS
 	SELECT cluster_id, p_name_key, var||'='||alt||':'||(1.0/cnt) AS altdef
 	FROM (
@@ -40,6 +46,8 @@ CREATE VIEW p_name_alts AS
 			FROM denorm_p_name_raw p JOIN p_name_vars v ON p.cluster_id=v.cluster_id
 		) AS x
 ;
+
+-- Add the random variable assignments to the dictionary
 
 -- Crashes the server
 -- UPDATE _dict
@@ -70,6 +78,15 @@ FROM dictstring
 WHERE name = 'wdc_dict'
 ;
 
-SELECT regexp_split_to_table( print(dict), ';\s*')
-FROM _dict 
-WHERE name = 'wdc_dict';
+-- SELECT regexp_split_to_table( print(dict), ';\s*')
+-- FROM _dict 
+-- WHERE name = 'wdc_dict';
+
+-- ERROR:  bdd_in: varnames cannot start with a digit: "50000000000000000000"
+CREATE MATERIALIZED VIEW denorm_p_name AS
+	SELECT p.*, bdd(a.altdef)
+	FROM denorm_p_name_raw p FULL OUTER JOIN p_name_alts a
+		 ON p.p_name_key = a.p_name_key
+	WHERE p.cluster_id=304
+;
+
